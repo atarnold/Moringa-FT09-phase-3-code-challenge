cb816f864a2c8fb63aaf53489b9d038a662da564
arnold <arnoldkmaina@gmail.com>
2024-06-10 21:51:57 +0300

    Moringa-FT09-phase-3-code-challenge

--- models/author.py
+++ models/author.py
@@ -1,3 +1,6 @@
+from database.connection import get_db_connection
+from models.article import Article
+
 class Author:
     def __init__(self, id, name):
         self.id = id
@@ -5,3 +8,14 @@ class Author:
 
     def __repr__(self):
         return f'<Author {self.name}>'
+
+    @property
+    def articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.id,))
+        articles_data = cursor.fetchall()
+        conn.close()
+        articles = [Article(article['id'], article['title'], article['content'], article['author_id'], article['magazine_id'])
+                    for article in articles_data]
+        return articles
--- tests/test_models.py
+++ tests/test_models.py
@@ -13,8 +13,20 @@ class TestModels(unittest.TestCase):
         self.assertEqual(article.title, "Test Title")
 
     def test_magazine_creation(self):
-        magazine = Magazine(1, "Tech Weekly")
+        magazine = Magazine(1, "Tech Weekly", "Technology")
         self.assertEqual(magazine.name, "Tech Weekly")
+
+    def test_author_articles(self):
+        author = Author(1, "John Doe")
+        articles = author.articles
+        self.assertIsInstance(articles, list)
+        self.assertTrue(all(isinstance(article, Article) for article in articles))
+
+    def test_magazine_contributing_authors(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        authors = magazine.contributing_authors
+        self.assertIsInstance(authors, list)
+        self.assertTrue(all(isinstance(author, Author) for author in authors))
 
 if __name__ == "__main__":
     unittest.main()
